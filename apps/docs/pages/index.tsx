60ed36af2f6d86a8cd4fdf75805f09b0b7e5658a
e-roy <ericroupe@gmail.com>
2022-07-29 19:04:29 -0400

    Revert "working pg, docs, sb"
    This reverts commit 588d4957a177b082d42b3ee09027041d5d9c3ca7.

--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -1,69 +1,10 @@
-import { useState, useEffect } from 'react';
-import {
-  darkTheme,
-  Address,
-  Button,
-  Badge,
-  DropdownMenu,
-  DropdownMenuTrigger,
-  DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuItem,
-  DropdownMenuSeparator,
-  DropdownMenuCheckboxItem,
-  DropdownMenuLabel,
-  DropdownMenuRadioGroup,
-  DropdownMenuRadioItem,
-} from 'test-turbo';
+import { Button } from 'test-turbo';
 
 export default function Docs() {
-  const [theme, setTheme] = useState('theme-default');
-
-  useEffect(() => {
-    document.body.classList.remove('theme-default', darkTheme);
-    document.body.classList.add(theme);
-  }, [theme]);
-
   return (
     <div>
-      <header>
-        <button
-          onClick={() =>
-            setTheme(
-              theme === 'theme-default' ? darkTheme.className : 'theme-default'
-            )
-          }
-        >
-          {theme === 'theme-default' ? 'Dark' : 'Light'}
-        </button>
-      </header>
       <h1>Docs</h1>
-      <Button size={'md'}>helloo</Button>
-      <Badge variant={'lime'}>badge</Badge>
-      <Address value="0xb8c2C29ee19D8307cb7255e1Cd9CbDE883A267d5" />
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button>Dropdown</Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align="end">
-          <DropdownMenuGroup>
-            <DropdownMenuItem>Item</DropdownMenuItem>
-            <DropdownMenuItem>Item</DropdownMenuItem>
-            <DropdownMenuItem>Item</DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuCheckboxItem>Item</DropdownMenuCheckboxItem>
-            <DropdownMenuCheckboxItem checked>Item</DropdownMenuCheckboxItem>
-            <DropdownMenuCheckboxItem>Item</DropdownMenuCheckboxItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuLabel>Choose one</DropdownMenuLabel>
-            <DropdownMenuRadioGroup value="one">
-              <DropdownMenuRadioItem value="one">Item</DropdownMenuRadioItem>
-              <DropdownMenuRadioItem value="two">Item</DropdownMenuRadioItem>
-              <DropdownMenuRadioItem value="three">Item</DropdownMenuRadioItem>
-            </DropdownMenuRadioGroup>
-          </DropdownMenuGroup>
-        </DropdownMenuContent>
-      </DropdownMenu>
+      <Button>Button</Button>
     </div>
   );
 }
