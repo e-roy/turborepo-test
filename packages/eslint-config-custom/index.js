72b11eced123725ac4fd63db57d428e92f5bff17
e-roy <ericroupe@gmail.com>
2022-07-29 15:10:41 -0400

    fix eslint ci

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,7 +1,20 @@
 module.exports = {
-  extends: ["next", "prettier"],
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:jest/recommended',
+    'plugin:jest-dom/recommended',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+  },
   rules: {
-    "@next/next/no-html-link-for-pages": "off",
-    "react/jsx-key": "off",
+    '@next/next/no-html-link-for-pages': 'off',
+    'react/jsx-key': 'off',
   },
 };
